cc5653ca4bd982a02127934ef872e9c31001a6fb
Naad Ali <naadalijutt786@gmail.com>
2025-07-30 00:00:38 +0500

    final

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -7,13 +7,16 @@
     <title>NaadAli | Portfolio</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link rel="icon" href="{{ asset('assets/images/logo.png') }}" type="image/png">
+    <link rel="icon" href="https://naadali.onrender.com/assets/images/logo.png" type="image/png">
     <link
         href="https://fonts.googleapis.com/css2?family=Amaranth:ital,wght@0,400;0,700;1,400;1,700&family=Bebas+Neue&family=Caveat:wght@400..700&family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Monoton&family=Noto+Sans:ital,wdth,wght@0,62.5,100..900;1,62.5,100..900&family=Nunito:ital,wght@0,200..1000;1,200..1000&family=Onest:wght@100..900&family=Prompt:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
         rel="stylesheet">
     <link rel="stylesheet" href="https://naadali.onrender.com/assets/css/style.css">
+    <link rel="stylesheet" href="https://naadali.onrender.com/build/assets/app-1UQdrMZs.js">
+    <link rel="stylesheet" href="https://naadali.onrender.com/build/assets/app-DypmRwCW.css">
+    <link rel="stylesheet" href="https://naadali.onrender.com/build/assets/app-DypmRwCW.js">
 
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
 </head>
 
 <body>
@@ -34,8 +37,6 @@
 </body>
 {{-- <script src="https://code.jquery.com/jquery-3.7.1.min.js"
     integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script> --}}
-<script src="{{ asset('assets/js/jQuery.js') }}"></script>
-
-<script src="{{ asset('assets/js/script.js') }}"></script>
-
+<script src="https://naadali.onrender.com/assets/js/jQuery.js"></script>
+<script src="https://naadali.onrender.com/assets/js/script.js"></script>
 </html>
